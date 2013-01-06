9ce67c7e5f4529a3d4cf3ceea3fc127b44c6ada7
Jesus Roldan <xebuz18@gmail.com>
2013-01-06 13:11:02 -0200

    Update libs/game.js
    Corregido error de merge

--- libs/game.js
+++ libs/game.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var HEIGHT = 550
 var WIDTH = 650
 
@@ -10,14 +9,3 @@ function start(){
 	Crafty.scene("loading");
 
 };
-=======
-var HEIGHT = 550
-var WIDTH = 650
-
-
-function start(){
-	Crafty.init(WIDTH, HEIGHT);
-	Crafty.Canvas;
-	Crafty.scene("loading");
-};
->>>>>>> fe2af3b08f1f2896e90953d0d6534bc403ff3801
