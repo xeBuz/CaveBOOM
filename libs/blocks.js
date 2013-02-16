14dee5fdc24450cd1e1a8f66550774f8e6bb4e28
marilynpi <marilyn.pirrello@gmail.com>
2013-02-16 15:50:48 -0300

    Definidos los componentes Brick y Steel

--- libs/blocks.js
+++ libs/blocks.js
@@ -3,8 +3,38 @@ Crafty.c("Block", {
 
 	init: function(){
 		this.attr({w: this._size,
-                   h: this._size,})
-		this.requires("2D, DOM, Solid, Collision")
+                   h: this._size,});
+		this.requires("2D, DOM, Solid, Collision");
 		this.collision( new Crafty.polygon([0,0],[50,0],[50,50],[0,50]) );
+//		this.onHit("Fire", function(){
+//				this.destroy();
+//		});
+
+	},
+})
+
+Crafty.c("Brick", {
+	_size : 50,
+
+	init: function(){
+		this.attr({w: this._size,
+                   h: this._size,});
+		this.requires("2D, DOM, Solid, Collision");
+		this.collision( new Crafty.polygon([0,0],[50,0],[50,50],[0,50]) );
+		this.onHit("Fire", function(){
+				this.destroy();
+		});
+	},
+})
+
+Crafty.c("Steel", { 
+	_size : 50,
+
+	init: function(){
+		this.attr({w: this._size,
+                   h: this._size,});
+		this.requires("2D, DOM, Solid, Collision");
+		this.collision( new Crafty.polygon([0,0],[50,0],[50,50],[0,50]) );
+
 	},
 })
